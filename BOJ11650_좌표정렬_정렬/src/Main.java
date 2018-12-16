8a99fa5b6005b81e01ac5e17f0572cb351530622
나밍 <40882598+snt2525@users.noreply.github.com>
2018-12-17 03:45:41 +0900

    수정

--- BOJ11650_좌표정렬_정렬/src/Main.java
+++ BOJ11650_좌표정렬_정렬/src/Main.java
@@ -6,33 +6,37 @@ class Pair{
 		this.x = x;
 		this.y = y;
 	}
+	int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
 }
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
+		String[] a = {"A", "a"," "};
+		System.out.print(a[0].compareTo(a[1]));
 		int n = sc.nextInt();
 		Pair[] p = new Pair[n];
 		for(int i = 0;i < n;i++) 
 			p[i] = new Pair(sc.nextInt(), sc.nextInt());
-
+		
+		
 		Arrays.sort(p , new Comparator<Pair>() {
 			public int compare(Pair arg0, Pair arg1) {
+				if(arg0.x == arg1.x) {
+					return Integer.compare(arg0.y, arg1.y);
+				}
 				return Integer.compare(arg0.x, arg1.x);
 			}
 			
 		});
-		
-		Arrays.sort(p , new Comparator<Pair>() {
-			public int compare(Pair arg0, Pair arg1) {
-				return Integer.compare(arg0.y, arg1.y);
-			}
-			
-		});
-		
-		for(int i = 0;i < n;i++) {
+	
+		for(int i = 0;i < n;i++)
 			System.out.println(p[i].x + " "+p[i].y);
-		}
 	}
 }
